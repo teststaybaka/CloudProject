19d1155cbe975155377f72067e6227041845934d
Percee <pbg2111@columbia.edu>
2015-12-05 10:42:25 -0500

    Update Signin

--- static/signin.js
+++ static/signin.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
-		FB.getLoginStatus(function(response) {
-    	statusChangeCallback(response);
-    	});
-
 		$("#facebook").click(function(){
+			if(checkLoginState() === "connected")
+				return;
 			FB.login(function(response) {
   				if (response.status === 'connected') {
     			// Logged into your app and Facebook.
@@ -16,29 +14,22 @@ $(document).ready(function(){
 			});
 		})
 
-
 });
 
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
+
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      testAPI();
+      document.cookie="user_id="+response.authResponse.userID+";";
+      window.location.replace("https://hands-for-you.appspot.com/signin");
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
     }
   }
 
@@ -46,9 +37,12 @@ $(document).ready(function(){
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
   function checkLoginState() {
+  	var status = 'unknown';
     FB.getLoginStatus(function(response) {
       statusChangeCallback(response);
+      status = response.status;
     });
+    return status;
   }
 
   // Here we run a very simple test of the Graph API after login is
@@ -57,7 +51,5 @@ $(document).ready(function(){
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
     });
   }

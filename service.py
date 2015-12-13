872e596b88b9e0ec09584bb68e44754aeec71ab2
teststaybaka <teststaybaka@gmail.com>
2015-12-13 18:56:38 -0500

    change service.py

--- service.py
+++ service.py
@@ -20,13 +20,8 @@ class SearchServices(BaseHandler):
     def get(self):
         keywords = self.get_keywords()
         query_string = ''
-        flag = True
-        if not keywords:
-            flag = False
-            
-        if flag:
+        if keywords:
             query_string = 'content: '+ keywords
-
             
             status = self.request.get('status')
             if status:
@@ -58,69 +53,6 @@ class SearchServices(BaseHandler):
 
                 query_string += ' AND distance(location, geopoint('+latitude+', '+longitude+')) < '+distance
 
-
-        status = self.request.get('status')
-        if status:
-            query_string += ' AND status = '+status
-
-        price_min = self.request.get('price_min')
-        if price_min:
-            query_string += ' AND price >= '+price_min
-
-        price_max = self.request.get('price_max')
-        if price_max:
-            query_string += ' AND price <= '+price_max
-
-        kind = self.request.get('kind')
-        if kind:
-            query_string += ' AND kind = '+kind
-
-        address = self.request.get('address')
-        if address:
-            try:
-                loc_results = getGeolocation(self.address)
-                self.latitude = loc_results[0]['geometry']['location']['lat']
-                self.longitude = loc_results[0]['geometry']['location']['lng']
-                distance = self.request.get('distance')
-                t = float(distance)
-            except ValueError:
-                self.redirect(self.render('notify', {'message': 'Address or distance invalid.'}))
-                return
-
-            query_string += ' AND distance(location, geopoint('+latitude+', '+longitude+')) < '+distance
-
-
-        status = self.request.get('status')
-        if status:
-            query_string += ' AND status = "'+status+'"'
-
-        price_min = self.request.get('price_min')
-        if price_min:
-            query_string += ' AND price >= '+price_min
-
-        price_max = self.request.get('price_max')
-        if price_max:
-            query_string += ' AND price <= '+price_max
-
-        kind = self.request.get('kind')
-        if kind:
-            query_string += ' AND kind = '+kind
-
-        address = self.request.get('address')
-        if address:
-            try:
-                loc_results = getGeolocation(self.address)
-                self.latitude = loc_results[0]['geometry']['location']['lat']
-                self.longitude = loc_results[0]['geometry']['location']['lng']
-                distance = self.request.get('distance')
-                t = float(distance)
-            except ValueError:
-                self.redirect(self.render('notify', {'message': 'Address or distance invalid.'}))
-                return
-
-            query_string += ' AND distance(location, geopoint('+latitude+', '+longitude+')) < '+distance
-
-
         options = search.QueryOptions(limit=20, ids_only=True)
         query = search.Query(query_string=query_string, options=options)
         index = search.Index(name='services_by_created')
@@ -129,7 +61,7 @@ class SearchServices(BaseHandler):
         total_found = result.number_found
         services = ndb.get_multi([ndb.Key(urlsafe=doc.doc_id) for doc in result.results])
         creators = ndb.get_multi([service.creator for service in services])
-        self.render('findService', {'services': services, 'creators': creators})
+        self.render('findService', {'services': services, 'creators': creators, 'total_found': total_found})
 
 class ServiceHandle(BaseHandler):
     @login_required
